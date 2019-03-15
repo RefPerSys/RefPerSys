b37cb10d87072c29fd56f542bab6fe1a32496d8a
Abhishek Chakravarti <abhishek@taranjali.org>
2019-03-15 10:39:32 +0530

    Forward declare object type rps_object
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- inc/object.h
+++ inc/object.h
@@ -35,6 +35,22 @@
         extern "C" {
 #endif
 
+
+
+
+        /* forward-declare the object type; this type is analogous to bismon's
+         * object_stBM struct; I'm guessing that the object_stBM struct is
+         * treated as an opaque type through the objectval_tyBM type since doing
+         * a grep -r "object_stBM" on the bismon source code root directory
+         * doesn't seem to indicate it being used in any file other than the
+         * types_BM.h header; accordingly, I'm deferring (perhaps incorrectly)
+         * the definition of the rps_object type to the src/object.c file which
+         * will implement the object type interface's callable units */
+typedef struct __rps_object rps_object;
+
+
+
+
         /* close support for C++ */
 #if (defined __cplusplus)
         }
