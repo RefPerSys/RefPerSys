c1b5d2a30aa9cf31bbb2823472f8d708b0bfd383
Abhishek Chakravarti <abhishek@taranjali.org>
2019-03-16 18:57:30 +0530

    Define rps_assert_stdc() macro
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ inc/assert.h
@@ -0,0 +1,65 @@
+/*
+ * File: refpersys/inc/assert.h
+ *
+ * Description:
+ *      This file is part of the Reflective Persistent System. It declares the
+ *      assertion macros.
+ *
+ * Author(s):
+ *      Basile Starynkevitch <basile@starynkevitch.net>
+ *      Niklas Rosencrantz <niklasro@gmail.com>
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *
+ * Copyright:
+ *      (c) 2019 The Reflective Persistent System Team
+ *      <https://refpersys.gitlab.io>
+ *
+ * License:
+ *      Released under the GNU General Public License version 3 (GPLv3)
+ *      <http://opensource.org/licenses/GPL-3.0>. See the accompanying LICENSE
+ *      file for complete licensing details.
+ *
+ *      BY CONTINUING TO USE AND/OR DISTRIBUTE THIS FILE, YOU ACKNOWLEDGE THAT
+ *      YOU HAVE UNDERSTOOD THESE LICENSE TERMS AND ACCEPT THEM.
+ */
+
+
+
+
+        /* create header guard */
+#if (!defined __REFPERSYS_ASSERTMACROS_DEFINED)
+#       define __REFPERSYS_ASSERTMACROS_DEFINED
+
+
+
+
+        /*  open support for C++ */
+#if (defined __cplusplus)
+        extern "C" {
+#endif
+
+
+
+
+/*
+ *      rps_assert_stdc() - checks C99 and above available
+ */
+#if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L)
+#       define rps_assert_stdc()
+#else
+#       error "building refpersys requires a C99 compiler"
+#endif
+
+
+
+
+        /* close support for C++ */
+#if (defined __cplusplus)
+        }
+#endif
+
+
+
+
+#endif /* (!defined __REFPERSYS_ASSERTMACROS_DEFINED) */
+
