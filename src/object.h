ad37a58b8a4799f69377122d1c1f09935ed71ff4
Basile Starynkevitch <basile@starynkevitch.net>
2019-03-17 15:24:30 +0100

    Added GPLv3+ standard comment.
    Renamed rps_object_spawn as rps_object_make. Commented it a bit more.
    
    Declared rps_object_make_with_objid.

--- src/object.h
+++ src/object.h
@@ -3,7 +3,7 @@
  *
  * Description:
  *      This file is part of the Reflective Persistent System. It declares the
- *      object ype and its interface.
+ *      object type and its interface.
  *
  * Author(s):
  *      Basile Starynkevitch <basile@starynkevitch.net>
@@ -18,10 +18,22 @@
  *      Released under the GNU General Public License version 3 (GPLv3)
  *      <http://opensource.org/licenses/GPL-3.0>. See the accompanying LICENSE
  *      file for complete licensing details.
+ *-----------------
  *
- *      BY CONTINUING TO USE AND/OR DISTRIBUTE THIS FILE, YOU ACKNOWLEDGE THAT
- *      YOU HAVE UNDERSTOOD THESE LICENSE TERMS AND ACCEPT THEM.
- */
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****/
 
 
 
@@ -30,16 +42,11 @@
 #if (!defined __REFPERSYS_OBJECTID_DEFINED)
 #       define __REFPERSYS_OBJECTID_DEFINED
 
-
-
-
         /* include required header files */
 #include <stdint.h>
 
 
 
-
-        /*  open support for C++ */
 #if (defined __cplusplus)
         extern "C" {
 #endif
@@ -55,28 +62,41 @@
          * types_BM.h header; accordingly, I'm deferring (perhaps incorrectly)
          * the definition of the rps_object type to the src/object.c file which
          * will implement the object type interface's callable units */
-typedef struct __rps_object rps_object;
+typedef struct rps_object_st rps_object;
 
+typedef struct rps_objid_st rps_objid;
 
+/* TODO: did we really decide that objids are 128 bits? They might be a bit smaller (96 bits)! */
+struct rps_objid_st { rps_serial63 oid_hi, oid_low; };
 
+struct rps_object_st {
+#warning TODO: rps_object_st is very incomplete
+  /* some fields are needed before ob_id */
+  const rps_objid ob_id;
+  /* many fields are needed after ob_id */
+};
+ 
 
 /*
- *      rps_object_spawn() - spawns new object
+ *      rps_object_make() - make a fresh new object
  *        - obj: contextual object
  *
- *      The rps_object_spawn() interface function creates a new object instance
+ *      The rps_object_make() interface function creates a new object instance
  *      @obj through the factory pattern.
+ * 
+ *       rps_object_make_with_objid is creating an object with a given
+ *       oid and check that the given oid is valid and was not already
+ *       used.
  *
- *      See:
- *        - bismon makeobj_BM()
- *        - bismon_makeobjofid_BM()
+ *    On out-of-memory condition (a very rare event) these functions panic and abort.
+ *    But both functions are using MPS allocation.
  */
-extern void rps_object_spawn(rps_object *obj);
+extern rps_object* rps_object_make(void);
+extern rps_object* rps_object_make_with_objid(rps_objid oid);
 
 
 
 
-        /* close support for C++ */
 #if (defined __cplusplus)
         }
 #endif
