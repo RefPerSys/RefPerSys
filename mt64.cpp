1978f2ec4cb3fb90bb10a09ce21e796e54514084
niklasr <niklasr@protonmail.com>
2019-03-17 05:45:45 +0100

    Make extern C and attempt to reseed

--- mt64.cpp
+++ mt64.cpp
@@ -1,11 +1,22 @@
 #include <random>
 #include <iostream>
-int main()
+#include <ctime>
+
+extern "C" {
+   uint64_t rps_random_uint64(void);
+}
+static thread_local int counter = 0;
+uint64_t rps_random_uint64(void)
 {
-  std::random_device rd;
-  std::mt19937_64 gen(rd());
-  std::uniform_int_distribution<unsigned long long> dis;
-  std::cout << dis(gen) << ' ';
-  std::cout << std::endl;
-  return 0;
+    counter++;
+    std::cout << counter << "counter ";
+    std::random_device rd;
+    std::mt19937 g{ rd() };
+    if (counter >= 65536 )
+      g.seed(time(NULL));
+    std::mt19937_64 gen(rd());
+    std::uniform_int_distribution<unsigned long long> dis;
+    std::cout << dis(gen) << ' ';
+    std::cout << std::endl;
+    return 0;
 }
