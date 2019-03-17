4ba64c51dcec67822cc00b1d91a76cc4f4e12f91
Niklas Rosencrantz <niklasr@protonmail.com>
2019-03-17 16:13:23 +0000

    Update mt64.c

--- src/mt64.c
+++ src/mt64.c
@@ -1,7 +1,7 @@
 #define __STDC_FORMAT_MACROS 1
 #include "../inc/mt64.hpp"
 #include <inttypes.h>
-int main (void) {
+int testmt64 (void) {
 	    printf("%" PRIu64 "\n", rps_random_uint64());
 	        return 0;
 }
