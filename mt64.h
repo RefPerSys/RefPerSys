d2910ee8f0fd2052798ec6eb16dfa48ab0c1a9b9
niklasr <niklasr@protonmail.com>
2019-03-17 08:45:24 +0100

    Address issue https://gitlab.com/refpersys/refpersys/issues/8

--- mt64.h
+++ mt64.h
@@ -9,11 +9,11 @@ static thread_local int counter = 0;
 uint64_t rps_random_uint64(void)
 {
     counter++;
-    std::random_device rd;
-    std::mt19937 g{ rd() };
+    static thread_local std::random_device rd;
+    static thread_local std::mt19937 g{ rd() };
     if (counter >= 65536 )
       g.seed(time(NULL));
-    std::mt19937_64 gen(rd());
-    std::uniform_int_distribution<unsigned long long> dis;
+    static thread_local std::mt19937_64 gen(rd());
+    static thread_local std::uniform_int_distribution<unsigned long long> dis;
     return dis(gen);
 }
