2e725fb78107a652a49371f4857512d9b62f844a
Abhishek Chakravarti <abhishek@taranjali.org>
2019-03-25 18:52:13 +0530

    Define branch prediction macros
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- inc/util.h
+++ inc/util.h
@@ -42,5 +42,20 @@ extern "C" const char rps_build_timestamp[];
 /* Mersenne twister */
 extern "C" uint64_t rps_random_uint64(void);
 
+
+
+// branch prediction macro to indicate unlikely condition
+// adapted from MOM_UNLIKELY() macro in meltmoni.hh
+#define rps_unlikely(p) __builtin_expect(!!(P),0)
+
+
+
+// branch prediction macro to indicate likely condition
+// adapted from MOM_LIKELY() macro in meltmoni.hh
+#define rps_likely(p) !__builtin_expect(!(P),0)
+
+
+
+
 #endif /* REFPERSYS_UTIL_INCLUDED */
 
