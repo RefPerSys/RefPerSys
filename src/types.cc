5e4b2982be32f45f7ce175c7bd0de93436cc5a0c
Basile Starynkevitch <basile@starynkevitch.net>
2019-03-27 19:02:23 +0100

    good code from Abhishek in commit  404563b666358. Basile added a few comments in src/types.cc

--- src/types.cc
+++ src/types.cc
@@ -1,17 +1,25 @@
 #include "types.h"
 
 
-// initialise static members of Rps_Value_Data_Mostly_Copying
+// declare static members of Rps_Value_Data_Mostly_Copying
 thread_local mps_arena_t Rps_Value_Data_Mostly_Copying::_arena = nullptr;
 thread_local mps_pool_t Rps_Value_Data_Mostly_Copying::_pool = nullptr;
 thread_local mps_ap_t Rps_Value_Data_Mostly_Copying::_allocpt = nullptr;
 
 
-// initialise MPS arena
+// initialize MPS arena
 // the arena is requested from the OS
-void Rps_Value_Data_Mostly_Copying::init_arena(void)
+void 
+Rps_Value_Data_Mostly_Copying::init_arena(void)
 {
-  const size_t arenasize = 32ul * 1024 * 1024;
+  // we may have to tune that `arenasize` a bit. Basile's guess is
+  // that it should be related to the size of the L3 cache, and then
+  // that number might be slightly too large. But that should be done
+  // much later, when we do have some code actively using the MPS GC.
+  const size_t arenasize = 32ul * 1024 * 1024; // 32 Megabytes
+  // TODO: explain what happens when the 32 megabytes are exhausted?
+  // is a new "arena" or "pool" or some 32 megabytes memory zone
+  // created? How? When?
   mps_arg_s args[1];
   mps_res_t res;
 
@@ -36,15 +44,20 @@ void Rps_Value_Data_Mostly_Copying::init_arena(void)
                           mps_message_type_finalization());
 }
 
-
 // initialise MPS pool as Automatic Mostly Copying Type
 // TODO: need Dr. Basile's advice on object format and object chains; for the
 // time being, mps_pool_create_k() is being passed mps_args_none, but probably
 // need need to be passed object format and object chain parameters
-void Rps_Value_Data_Mostly_Copying::init_pool(void)
+void 
+Rps_Value_Data_Mostly_Copying::init_pool(void)
 {
-  // TODO: do we need to create object format?
-  // TODO: do we need to create object chain?
+  // Basile thinks: if that short routine is called *only* from
+  // Rps_Value_Data_Mostly_Copying::init_mps we could move its code
+  // there.
+  
+  // TODO: do we need to create object format? Basile don't know yet. That could be decided later.
+  // TODO: do we need to create object chain? Basile don't know yet. That could be decided later.
+  // Basile reminds: the priority is the primordial persistence milestone.
 
   mps_res_t res = mps_pool_create_k(&Rps_Value_Data_Mostly_Copying::_pool,
                                     Rps_Value_Data_Mostly_Copying::_arena,
@@ -62,7 +75,8 @@ void Rps_Value_Data_Mostly_Copying::init_pool(void)
 // initialise MPS allocation point
 // the allocation point allows fast inline allocation for objects from the MPS
 // pool
-void Rps_Value_Data_Mostly_Copying::init_ap(void)
+void 
+Rps_Value_Data_Mostly_Copying::init_ap(void)
 {
   mps_res_t res = mps_ap_create_k(&Rps_Value_Data_Mostly_Copying::_allocpt,
                                   Rps_Value_Data_Mostly_Copying::_pool,
@@ -76,11 +90,17 @@ void Rps_Value_Data_Mostly_Copying::init_ap(void)
 }
 
 
+
+
 // initialise MPS area, pool, and allocation pointer
-void Rps_Value_Data_Mostly_Copying::init_mps(void)
+void 
+Rps_Value_Data_Mostly_Copying::init_mps(void)
 {
+  // Basile thinks: If these routines are only called here, we might want to move
+  // their code inside. IMHO a 80 lines routine stays readable, but
+  // needs a few (perhaps 3 or 4) lines of explanation.
   Rps_Value_Data_Mostly_Copying::init_arena();
   Rps_Value_Data_Mostly_Copying::init_pool();
   Rps_Value_Data_Mostly_Copying::init_ap();
-}
+} /*end Rps_Value_Data_Mostly_Copying::init_mps*/
 
