4b4c62044e18fd24b15b78595028b0ab0b9ae7f8
Basile Starynkevitch <basile@starynkevitch.net>
2019-06-02 20:09:33 +0200

    Rps_SetValue::tiny_benchmark_1 still incomplete, but using rop - random operation
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- minitests_rps.cc
+++ minitests_rps.cc
@@ -55,6 +55,7 @@ Rps_SetValue::tiny_benchmark_1(Rps_CallFrameZone*callingfra, unsigned num)
                  Rps_Value arrval[benchsize];
                  Rps_ObjectRef arrob[benchsize];
                 );
+  assert (num > 10);
   double t0 = 0.0, t1 = 0.0, t2 = 0.0, t3 = 0.0, t4 = 0.0, t5 = 0.0, t6 = 0.0;
   t0 = rps_thread_cpu_time();
   assert (t0 > 0.0);
@@ -65,14 +66,57 @@ Rps_SetValue::tiny_benchmark_1(Rps_CallFrameZone*callingfra, unsigned num)
       if (i == benchsize/2)
         t1 = rps_thread_cpu_time();
     }
+  assert (benchsize < 0xff);
   t2 = rps_thread_cpu_time();
   std::cout << "tiny_benchmark_1 (" << __FILE__ << ":" << __LINE__
             << ") made " << (2*benchsize) << " objects in "
             << (t2-t0) << " thread-CPU seconds, half of them in "
             << (t1-t0)  << " thread-CPU seconds, so "
             << 1.0e6*(t2-t0)/(2*benchsize) << " µsec/obj." << std::endl;
-#warning unimplemented Rps_SetValue::tiny_benchmark_1
-  RPS_FATAL("unimplemented Rps_SetValue::tiny_benchmark_1 num=%u", num);
+  for (unsigned cnt=0; cnt<num; cnt++)
+    {
+      auto rop = Rps_Random::random_quickly_4bits();
+      assert (rop >= 0 && rop < 16);
+      switch(rop)
+        {
+        case 0:
+          _.arrval[Rps_Random::random_quickly_8bits() % benchsize]
+            = Rps_Value(Rps_ObjectRef::make(RPS_CURFRAME));
+          break;
+        case 1:
+        case 2:
+          _.arrval[Rps_Random::random_32u() % benchsize]
+            = Rps_Value(_.arrob[Rps_Random::random_32u() % benchsize]);
+          break;
+        case 3:
+        case 4:
+          _.arrval[Rps_Random::random_32u() % benchsize]
+            = Rps_IntValue(Rps_Random::random_quickly_8bits() - 100);
+          break;
+        case 5:
+        case 6:
+        {
+          Rps_Id randid (nullptr);
+          char cbuf[24];
+          memset (cbuf, 0, sizeof(cbuf));
+          randid.to_cbuf24(cbuf);
+          cbuf[ Rps_Random::random_quickly_4bits() + 4] = (char)0;
+          _.arrval[Rps_Random::random_32u() % benchsize]
+            = Rps_StringValue(RPS_CURFRAME,cbuf+1);
+          break;
+        }
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+          break;
+        }
+    }
 } // end Rps_SetValue::tiny_benchmark_1
 
 
