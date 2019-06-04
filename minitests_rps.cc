18ecade9b804b03e258af0d26431dec12b62c916
Basile Starynkevitch <basile@starynkevitch.net>
2019-06-04 07:58:29 +0200

    more in Rps_SetValue::tiny_benchmark_1, it is still incomplete and needs to test removal of attributes, of components, etc...
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- minitests_rps.cc
+++ minitests_rps.cc
@@ -73,6 +73,8 @@ Rps_SetValue::tiny_benchmark_1(Rps_CallFrameZone*callingfra, unsigned num)
             << (t2-t0) << " thread-CPU seconds, half of them in "
             << (t1-t0)  << " thread-CPU seconds, so "
             << 1.0e6*(t2-t0)/(2*benchsize) << " µsec/obj." << std::endl;
+  /// think of this tiny_benchmark_1 as some "bytecode" machine
+  /// executing a random stream of bytecodes.
   for (unsigned cnt=0; cnt<num; cnt++)
     {
       auto rop = Rps_Random::random_quickly_4bits();
@@ -80,34 +82,40 @@ Rps_SetValue::tiny_benchmark_1(Rps_CallFrameZone*callingfra, unsigned num)
       switch(rop)
         {
         case 0:
+          // create a random new object
           _.arrval[Rps_Random::random_quickly_8bits() % benchsize]
             = Rps_Value(Rps_ObjectRef::make(RPS_CURFRAME));
           break;
         case 1:
         case 2:
+          // fetch an existing object as random value
           _.arrval[Rps_Random::random_quickly_8bits() % benchsize]
             = Rps_Value(_.arrob[Rps_Random::random_quickly_8bits()
                                 % benchsize]);
           break;
         case 3:
         case 4:
+          // create a random small tagged int
           _.arrval[Rps_Random::random_quickly_8bits() % benchsize]
             = Rps_IntValue(Rps_Random::random_quickly_8bits() - 100);
           break;
         case 5:
         case 6:
+          // create a random string value
         {
           Rps_Id randid (nullptr);
           char cbuf[24];
           memset (cbuf, 0, sizeof(cbuf));
           randid.to_cbuf24(cbuf);
-          cbuf[ Rps_Random::random_quickly_4bits() + 4] = (char)0;
+          cbuf[0] = ' ' + (Rps_Random::random_quickly_8bits() % 64 + 1);
+          cbuf[Rps_Random::random_quickly_4bits() + 4] = (char)0;
           _.arrval[Rps_Random::random_quickly_8bits() % benchsize]
-            = Rps_StringValue(RPS_CURFRAME,cbuf+1);
+            = Rps_StringValue(RPS_CURFRAME,cbuf);
           break;
         }
         case 7:
         case 8:
+          // put a random attribute
         {
           auto& destob =
             _.arrob[Rps_Random::random_quickly_8bits() % benchsize];
@@ -118,6 +126,7 @@ Rps_SetValue::tiny_benchmark_1(Rps_CallFrameZone*callingfra, unsigned num)
           break;
         }
         case 9:
+          // append a random component
         {
           auto& destob =
             _.arrob[Rps_Random::random_quickly_8bits() % benchsize];
@@ -128,6 +137,21 @@ Rps_SetValue::tiny_benchmark_1(Rps_CallFrameZone*callingfra, unsigned num)
           break;
         }
         case 10:
+          // if possible, swap a random _.arrob entry with a random
+          // _.arrval entry (if it holds an object)
+        {
+          auto& destob =
+            _.arrob[Rps_Random::random_quickly_8bits() % benchsize];
+          auto& othval =   _.arrval[Rps_Random::random_quickly_8bits() % benchsize];
+          if (destob && othval && othval.as_object() != nullptr)
+            {
+              Rps_ObjectRef rdestob = destob;
+              Rps_Value vothval = othval;
+              destob = vothval.as_object();
+              othval =  Rps_Value(rdestob);
+            };
+        }
+        break;
         case 11:
         case 12:
         case 13:
