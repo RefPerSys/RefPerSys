8dec38c9b65b6602eed6a7256a98bbe1ed07755c
Basile Starynkevitch <basile@starynkevitch.net>
2019-10-18 12:39:15 +0200

    adding bootstrapping

--- doc/design-ideas/build.sh
+++ doc/design-ideas/build.sh
@@ -5,4 +5,6 @@ lualatex --shell-escape  --halt-on-error refpersys-design
 biber refpersys-design
 lualatex --shell-escape  --halt-on-error refpersys-design
 [ -f refpersys-design.idx ] && texindy -v -C utf8 -I latex refpersys-design.idx
-lualatex --shell-escape  --halt-on-error refpersys-design
+if lualatex --shell-escape  --halt-on-error refpersys-design ; then
+    [ -x $HOME/bin/install-refpersys-design-report.sh ] && $HOME/bin/install-refpersys-design-report.sh
+fi
