15d8d663e5f1ce80fbc8cc50b4557d3afd8a2bfe
Basile Starynkevitch <basile@starynkevitch.net>
2019-10-19 10:53:12 +0200

    add CC-BY-SA-4 license

--- doc/design-ideas/build.sh
+++ doc/design-ideas/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 echo $0 starting in $(pwd)
 rm -vf  refpersys-design.{aux,bcf,blg,log,run.xml,toc}
+for svgfile in ../CC-BY-SA-icon.svg ; do
+    svgbase=$(basename $svgfile .svg)
+    inkscape --without-gui --export-pdf=$svgbase.pdf $svgfile
+    inkscape --without-gui --export-eps=$svgbase.eps $svgfile
+done
 lualatex --shell-escape  --halt-on-error refpersys-design
 biber refpersys-design
 lualatex --shell-escape  --halt-on-error refpersys-design
