90d1b9795e93f69e4d44c1a7fbf4429dc9e21e95
Basile Starynkevitch <basile@starynkevitch.net>
2019-11-04 14:55:26 +0100

    incomplete dot-classhier.dot shown

--- doc/design-ideas/build.sh
+++ doc/design-ideas/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 echo $0 starting in $(pwd)
-rm -vf  refpersys-design.{aux,bcf,blg,log,run.xml,toc}
+rm -vf  refpersys-design.{aux,bcf,blg,log,run.xml,toc} dot*.{eps,pdf,svg}
 
 ## see http://inkscape.org/
+printf "\n\n ================ SVG processing by inkscape of "; echo *.svg "files."
 for svgfile in ../CC-BY-SA-icon.svg \
 		   spiral-model-softdevel.svg spiral-stairs.svg \
 	       heap-refpersys.svg ; do
@@ -12,14 +13,17 @@ for svgfile in ../CC-BY-SA-icon.svg \
 done
 
 ## see http://graphviz.org/
-for dotfile in *.dot ; do
+printf "\n\n ================ dot processing by graphviz of "; echo  dot*.dot "files."
+ls -ls dot*.dot
+for dotfile in dot*.dot ; do
     dotbase=$(basename $dotfile .dot)
-    dot $dotfile -O -o$dotbase.eps $dotfile
-    dot $dotfile -O -o$dotbase.pdf $dotfile
-    dot $dotfile -O -o$dotbase.svg $dotfile
+    dot -v -Teps -o $dotbase.eps  $dotfile
+    dot -v -Tpdf -o $dotbase.pdf  $dotfile
+    dot -v -Tsvg -o $dotbase.svg  $dotfile
 done
+ls -ls --sort=time dot*
 
-
+printf "\n\n ================ latexing and bibering refpersys-design\n"
 lualatex --shell-escape  --halt-on-error refpersys-design
 biber refpersys-design
 lualatex --shell-escape  --halt-on-error refpersys-design
