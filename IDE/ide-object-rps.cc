95f97489a3d33680624eb00040c465bb3b1cf882
Basile Starynkevitch <basile@starynkevitch.net>
2019-11-10 09:22:16 +0100

    adding still empty ide-composite-rps.cc & ide-object-rps.cc

---
+++ IDE/ide-object-rps.cc
@@ -0,0 +1,43 @@
+/****************************************************************
+ * file ide-composite-rps.cc
+ *
+ * Description:
+ *      This file is part of the Reflective Persistent System IDE
+ *
+ *      It has code for objectsd
+ *
+ * Author(s):
+ *      Basile Starynkevitch <basile@starynkevitch.net>
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *      Nimesh Neema <nimeshneema@gmail.com>
+ *
+ *      © Copyright 2019 The Reflective Persistent System Team
+ *      team@refpersys.org
+ *
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include "ide-refpersys.hh"
+
+
+#ifndef RPS_GITID
+#error RPS_GITID should be set in compilation command
+#endif /*no RPS_GITID*/
+
+const char rpside_object_gitid[] = RPS_GITID;
+const char rpside_object_date[] = __DATE__; 
+
+
+//////////////// end of file ide-object-rps.cc
