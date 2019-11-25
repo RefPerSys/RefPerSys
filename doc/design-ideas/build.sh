43362605ad2cc1d4a3abe2990cb84fe15489096e
Basile Starynkevitch <basile@starynkevitch.net>
2019-11-25 19:54:48 +0100

    the refpersys-design.pdf builds again,  with improved build.sh and restored dot-classhier.dot  in oc/design-ideas/

--- doc/design-ideas/build.sh
+++ doc/design-ideas/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 echo $0 starting in $(pwd)
-rm -vf  refpersys-design.{aux,bcf,blg,log,run.xml,toc}
+rm -vf  refpersys-design.{aux,bcf,blg,log,run.xml,toc} dot*.{eps,pdf,svg}
+
+################
+## see http://inkscape.org/
+printf "\n\n ================ SVG processing by inkscape of "; echo *.svg "files."
+
 for svgfile in ../CC-BY-SA-icon.svg \
 		   spiral-model-softdevel.svg spiral-stairs.svg \
 	       heap-refpersys.svg ; do
@@ -8,6 +13,24 @@ for svgfile in ../CC-BY-SA-icon.svg \
     inkscape --without-gui --export-pdf=$svgbase.pdf $svgfile
     inkscape --without-gui --export-eps=$svgbase.eps $svgfile
 done
+
+################
+## see http://graphviz.org/
+printf "\n\n ================ dot processing by graphviz of "; echo  dot*.dot "files."
+ls -ls dot*.dot
+for dotfile in dot*.dot ; do
+    dotbase=$(basename $dotfile .dot)
+    dot -v -Teps -o $dotbase.eps  $dotfile
+    dot -v -Tpdf -o $dotbase.pdf  $dotfile
+    dot -v -Tsvg -o $dotbase.svg  $dotfile
+done
+ls -ls --sort=time dot*
+
+
+
+################
+printf "\n\n ================ latexing and bibering refpersys-design\n"
+
 lualatex --shell-escape  --halt-on-error refpersys-design
 biber refpersys-design
 lualatex --shell-escape  --halt-on-error refpersys-design
