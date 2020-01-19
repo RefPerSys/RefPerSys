81c96221acfb94353c57999b2787637c49388692
Basile Starynkevitch <basile@starynkevitch.net>
2020-01-19 16:20:31 +0100

    adding generated/rps-constants.hh

---
+++ generated/rps-constants.hh
@@ -0,0 +1,30 @@
+//: GENERATED file generated/rps-constants.hh / DO NOT EDIT!
+//: This file is part of the Reflective Persistent System.
+//:  © Copyright 2019 - 2020 The Reflective Persistent System Team.
+//:  see http://refpersys.org/ and contact team@refpersys.org for more.
+//: _
+//: This program is free software: you can redistribute it and/or modify
+//: it under the terms of the GNU General Public License as published by
+//: the Free Software Foundation, either version 3 of the License, or
+//: (at your option) any later version.
+//: _
+//: This program is distributed in the hope that it will be useful,
+//: but WITHOUT ANY WARRANTY; without even the implied warranty of
+//: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//: GNU General Public License for more details.
+//: _
+//: You should have received a copy of the GNU General Public License
+//: along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/// collection of constant objects, mentioned in C++ files, 
+///.... starting with rpskob followed by an oid.
+
+#ifndef RPS_INSTALL_CONSTANT_OB
+#error RPS_INSTALL_CONSTANT_OB(Oid) macro undefined
+#endif /*undefined RPS_INSTALL_CONSTANT_OB*/
+
+
+#undef  RPS_NB_CONSTANT_OB
+#define RPS_NB_CONSTANT_OB 0
+
+/// end of RefPerSys constants file generated/rps-constants.hh
