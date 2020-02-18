280de4e5b6dfc7310f6ab1e9e90d0fb749c7e31f
Basile Starynkevitch <basile@starynkevitch.net>
2020-02-18 15:31:50 +0100

    adding method set/display_value_qt _1568ZHTl0Pa00461I2

--- output_qrps.cc
+++ output_qrps.cc
@@ -253,6 +253,35 @@ rpsapply_33DFyPOJxbF015ZYoi (Rps_CallFrame*callerframe, ///
 } // end of rpsapply_33DFyPOJxbF015ZYoi
 
 
+// C++ closure for _1568ZHTl0Pa00461I2
+//!method set/display_value_qt
+extern "C" rps_applyingfun_t rpsapply_1568ZHTl0Pa00461I2;
+Rps_TwoValues
+rpsapply_1568ZHTl0Pa00461I2 (Rps_CallFrame*callerframe, ///
+                             const Rps_Value arg0, const Rps_Value arg1, ///
+                             const Rps_Value arg2, const Rps_Value arg3, ///
+                             const std::vector<Rps_Value>* restargs_ __attribute__((unused)))
+{
+  RPS_LOCALFRAME(rpskob_1568ZHTl0Pa00461I2,
+                 callerframe, //
+                 //Rps_Value arg0v;
+                 //Rps_Value arg1v;
+                 //Rps_Value arg2v;
+                 //Rps_Value arg3v;
+                 //Rps_ObjectRef obr;
+                 Rps_Value resmainv;
+                 Rps_Value resxtrav;
+                 //....etc....
+                );
+  // _.arg0v = arg0;
+  // _.arg1v = arg1;
+  // _.arg2v = arg2;
+  // _.arg3v = arg3;
+  ////==== body of _1568ZHTl0Pa00461I2 ====
+  ;
+  RPS_LOCALRETURNTWO(_.resmainv, _.resxtrav); // result of _1568ZHTl0Pa00461I2
+} // end of rpsapply_1568ZHTl0Pa00461I2
+
 
 
 /************************************************************* end of file output_qrps.cc ****/
