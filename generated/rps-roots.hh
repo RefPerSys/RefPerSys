4e253123c13ab0727b895cb589eb99f5d0fa10b2
Basile Starynkevitch <basile@starynkevitch.net>
2020-10-20 16:03:33 +0200

    Create the rps_serve_onion_expanded_stream object of class rps_routine using
    // see http://refpersys.org/
    // passed to commit 2663c870acef10a7b22  of RefPerSys (master branch)
    // GPLv3+ licensed
    // © Copyright Basile Starynkevitch <basile@starynkevitch.net>
    // once compiled, use it as:
    /// ./refpersys --plugin-after-load=/tmp/rpsplug_createonionexp.so --batch --dump=.
    
    void rps_do_plugin(const Rps_Plugin* plugin)
    {
      RPS_LOCALFRAME(/*descr:*/nullptr, /*callerframe:*/nullptr,
    		 Rps_ObjectRef obonionexp;
    		 Rps_Value strname;
    		 );
      RPS_INFORMOUT("running plugin " << plugin->plugin_name);
      // for descriptor of rps_serve_onion_expanded_stream
      _f.obonionexp = Rps_ObjectRef::make_object(&_,
    					     /*class:*/RPS_ROOT_OB(_7OrPRWQEg2o043XvK2), // rps_routine
    					     /*space:*/Rps_ObjectRef::root_space());
      _f.strname = Rps_String::make("rps_serve_onion_expanded_stream");
      _f.obonionexp->put_attr(RPS_ROOT_OB(_1EBVGSfW2m200z18rx),
    			    _f.strname);
      rps_add_root_object(_f.obonionexp);
    } // end rps_do_plugin
    
    /****************
     **                           for Emacs...
     ** Local Variables: ;;
     ** compile-command: "g++ -I$HOME/refpersys -std=gnu++17 -Wall -Wextra -g -shared -fPIC $HOME/tmp/rpsplug_createonionexp.cc -o /tmp/rpsplug_createonionexp.so" ;;
     ** End: ;;
     ****************/

--- generated/rps-roots.hh
+++ generated/rps-roots.hh
@@ -31,6 +31,7 @@ RPS_INSTALL_ROOT_OB(_0XMNvzdABUM03Bj7WP) //git_id∈symbol
 RPS_INSTALL_ROOT_OB(_0Z23UbC0G9b01WZZPN) //display_object_payload_web∈symbol
 RPS_INSTALL_ROOT_OB(_14M7WuJSWw702zB0M9) //"display_object_payload_web"∈named_selector
 RPS_INSTALL_ROOT_OB(_1aGtWm38Vw701jDhZn) //"the_agenda"∈agenda
+RPS_INSTALL_ROOT_OB(_1rfASGBBbFz02VUsMw) //"rps_serve_onion_expanded_stream"∈rps_routine
 RPS_INSTALL_ROOT_OB(_1wihX3eWD9o00QnxUX) //"our_contributors"∈mutable_set
 RPS_INSTALL_ROOT_OB(_1DBiKRl70P304DEMoR) //our_contributors∈symbol
 RPS_INSTALL_ROOT_OB(_1DUx3zfUzIb04lqNVt) //rps_window∈class
@@ -85,7 +86,7 @@ RPS_INSTALL_ROOT_OB(_9BnrMLXUhfG00llx8X) //function∈class
 RPS_INSTALL_ROOT_OB(_9Gz1oNPCnkB00I6VRS) //core_function∈class
 
 #undef RPS_NB_ROOT_OB
-#define RPS_NB_ROOT_OB 63
+#define RPS_NB_ROOT_OB 64
 
 #undef RPS_INSTALL_ROOT_OB
 /// end of RefPerSys roots file generated/rps-roots.hh
