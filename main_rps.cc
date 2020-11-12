1d635e9b745ac29f9ddb1b280f3212e0050b5a21
Basile Starynkevitch <basile@starynkevitch.net>
2020-11-12 14:24:29 +0100

    typo in help on --web program option

--- main_rps.cc
+++ main_rps.cc
@@ -153,8 +153,7 @@ struct argp_option rps_progoptions[] =
     /*key:*/ RPSPROGOPT_WEB, ///
     /*arg:*/ "HOST:PORT", ///
     /*flags:*/ 0, ///
-    /*doc:*/ "start web service as given on HOST:PORT, where -W. means --web=" RPS_DEFAULT_WEB_HOST_PORT //
-    RPS_DEFAULT_WEB_SERVICE, //
+    /*doc:*/ "start web service as given on HOST:PORT, where -W. means --web=" RPS_DEFAULT_WEB_HOST_PORT, //
     /*group:*/0 ///
   },
   /* ======= load a plugin after load ======= */
