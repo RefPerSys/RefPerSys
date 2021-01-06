951f04cbc0f99fbcc74da18f8cb1f611411d00de
Basile Starynkevitch <basile@starynkevitch.net>
2021-01-06 08:13:53 +0100

    adding empty cmdrepl_rps.cc

---
+++ cmdrepl_rps.cc
@@ -0,0 +1,43 @@
+/****************************************************************
+ * file cmdrepl_rps.cc
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Description:
+ *      This file is part of the Reflective Persistent System.
+ *
+ *      It implements the Read-Eval-Print-Loop commands in relation to
+ *      repl_rps.cc file.
+ *
+ * Author(s):
+ *      Basile Starynkevitch <basile@starynkevitch.net>
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *      Nimesh Neema <nimeshneema@gmail.com>
+ *
+ *      © Copyright 2021 The Reflective Persistent System Team
+ *      team@refpersys.org & http://refpersys.org/
+ *
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include "refpersys.hh"
+
+#include "readline/readline.h"
+#include "readline/history.h"
+
+extern "C" const char rps_cmdrepl_gitid[];
+const char rps_repl_gitid[]= RPS_GITID;
+
+extern "C" const char rps_cmdrepl_date[];
+const char rps_cmdrepl_date[]= __DATE__;
