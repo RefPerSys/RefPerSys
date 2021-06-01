0b1800707bcdaedb45ac714d3936aa6ae4023e32
Basile Starynkevitch <basile@starynkevitch.net>
2021-06-01 08:53:08 +0200

    call did_load_main_page_wrps from main page, and start coding object_input_keypress_event_wrps

--- webroot/js/refpersys.js
+++ webroot/js/refpersys.js
@@ -13,5 +13,31 @@
 //    Abhishek Chakravarti <abhishek@taranjali.org>
 //    Nimesh Neema <nimeshneema@gmail.com>
 //
-//    © Copyright 2020 The Reflective Persistent System Team
+//    © Copyright 2020 - 2021 The Reflective Persistent System Team
 //    team@refpersys.org & http://refpersys.org/
+
+// the Jquery framework has been loaded and can be used.
+
+/// By convention Javascript functions here have a name ending withj wrps
+
+/// Jquery function to be called on every keypress event for all input
+/// elements for a RefPerSys object.
+function object_input_keypress_event_wrps(ev,w)
+{
+    console.group("object_input_keypress_event_wrps");
+    console.debug("ev=", ev, " w=", w);
+    console.groupEnd();
+}				// end object_input_keypress_event_wrps
+
+
+
+
+/// the did_load_main_page_wrps is called by a script at end of main
+/// page (index.html.rps)
+function did_load_main_page_wrps()
+{
+    var showobj_inp = $('#showobj_inp_webrpsid');
+    showobj_inp.keypress(object_input_keypress_event_wrps);
+}		  // end did_load_main_page_wrps
+
+// end file webroot/js/refpersys.js of refpersys.org
