816bd54bab46a983ab294f756f640246bdee37d9
Basile Starynkevitch <basile@starynkevitch.net>
2021-07-03 10:03:17 +0200

    start adding empty tempgui-qrps.cc & tempgui-qrps.hh ; see https://framalistes.org/sympa/arc/refpersys-forum/2021-07/msg00002.html

---
+++ tempgui-qrps.hh
@@ -0,0 +1,44 @@
+/****************************************************************
+ * file tempgui-qrps.hh
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Description:
+ *
+ *      This file is part of the Reflective Persistent System.  It is
+ *      the header file for some optional dlopen-ed plugin using Qt5
+ *      It is tightly related to tempgui_qrps.cc
+ *      See on https://framalistes.org/sympa/arc/refpersys-forum/
+ *      the messages 2021-07/msg00002.html
+ *
+ * Author(s):
+ *      Basile Starynkevitch <basile@starynkevitch.net>
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *      Nimesh Neema <nimeshneema@gmail.com>
+ *
+ *      © Copyright 2021 The Reflective Persistent System Team
+ *      team@refpersys.org & http://refpersys.org/
+ *
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+
+#ifndef TEMPGUI_QRPS_INCLUDED
+#define TEMPGUI_QRPS_INCLUDED
+
+#include "refpersys.hh"
+
+
+#endif TEMPGUI_QRPS_INCLUDED
+//// end of file tempgui_qrps.hh for refpersys.org
