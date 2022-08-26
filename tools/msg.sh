4bbdf6037f3e157fe95a4b62f77bb54ea4ad7604
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-26 14:41:59 +0000

    Add file header
    The file header was missing from the tools/msg.sh file. This has
    now been fixed.
    
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/msg.sh
+++ tools/msg.sh
@@ -1,3 +1,36 @@
+################################################################################
+# file main_rps.cc
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Description:
+#      This file is part of the Reflective Persistent System.
+#
+#      It implements the agenda mechanism.
+#
+# Author(s):
+#      Basile Starynkevitch <basile@starynkevitch.net>
+#      Abhishek Chakravarti <abhishek@taranjali.org>
+#      Nimesh Neema <nimeshneema@gmail.com>
+#
+#      © Copyright 2020 The Reflective Persistent System Team
+#      team@refpersys.org & http://refpersys.org/
+#
+# License:
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+
 msg_ok()
 {
         ts=$(date +'%b %d %H:%M:%S')
