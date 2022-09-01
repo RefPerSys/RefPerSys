6867a23d2cdc6a1d75a58f3cd3e0d68e9413f345
Abhishek Chakravarti <abhishek@taranjali.org>
2022-09-01 16:42:11 +0000

    Add file header and fix typos
    The file header was missing from the tools/su.sh file; this has
    now been fixed. Additionally, there were two typos which have also
    been fixed.
    
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/su.sh
+++ tools/su.sh
@@ -1,7 +1,37 @@
-# This script defines the SU helper variable. Requires tools/msg.sh to be 
-# sourced.
+################################################################################
+# file tools/su.sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Description:
+#      This file is part of the Reflective Persistent System.
+#
+#      It defines the SU shell script variable as an abstraction over either
+#      sudo or doas. Requires that the tools/msg.sh file be sourced.
+#
+# Author(s):
+#      Basile Starynkevitch <basile@starynkevitch.net>
+#      Abhishek Chakravarti <abhishek@taranjali.org>
+#      Nimesh Neema <nimeshneema@gmail.com>
+#
+#      © Copyright 2020 The Reflective Persistent System Team
+#      team@refpersys.org & http://refpersys.org/
+#
+# License:
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
 
-# su command for client cod
+# su command for client code
 export SU
 
 # set SU to either sudo or doas, depending on which is available. sudo takes
@@ -11,5 +41,5 @@ if sudo -V >/dev/null 2>&1; then
 elif doas -L >/dev/null 2>&1; then
 	SU=doas
 else
-	msg_failt 'neither sudo nor doas found; install one of the two'
+	msg_fail 'neither sudo nor doas found; install one of the two'
 fi
