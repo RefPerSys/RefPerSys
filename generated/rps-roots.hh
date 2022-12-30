763e2aef048f5c62eca242d6593b926ec956949e
Basile Starynkevitch <basile@starynkevitch.net>
2022-12-30 11:19:55 +0100

    adding root object _2dvQOlSMjOu02zWx1n //repl_commutative_operator_class∈class ...

--- generated/rps-roots.hh
+++ generated/rps-roots.hh
@@ -49,6 +49,7 @@ RPS_INSTALL_ROOT_OB(_1NWEOIzo3WU03mE42Q) //rps_output_textedit∈class
 RPS_INSTALL_ROOT_OB(_1R1LKwGhoTr02e90bn) //web_handler∈class
 RPS_INSTALL_ROOT_OB(_1Win5yzaf1L02cBUlV) //"display_value_web"∈named_selector
 RPS_INSTALL_ROOT_OB(_1Zo12toMRTa03uqsXh) //display_object_occurrence_web∈symbol
+RPS_INSTALL_ROOT_OB(_2dvQOlSMjOu02zWx1n) //repl_commutative_operator_class∈class
 RPS_INSTALL_ROOT_OB(_2i66FFjmS7n03HNNBx) //space∈class
 RPS_INSTALL_ROOT_OB(_2wdmxJecnFZ02VGGFK) //repl_delimiter∈class
 RPS_INSTALL_ROOT_OB(_2A2mrPpR3Qf03p6o5b) //int∈class
@@ -114,7 +115,7 @@ RPS_INSTALL_ROOT_OB(_9BnrMLXUhfG00llx8X) //function∈class
 RPS_INSTALL_ROOT_OB(_9Gz1oNPCnkB00I6VRS) //core_function∈class
 
 #undef RPS_NB_ROOT_OB
-#define RPS_NB_ROOT_OB 92
+#define RPS_NB_ROOT_OB 93
 
 #undef RPS_INSTALL_ROOT_OB
 /// end of RefPerSys roots file generated/rps-roots.hh
--- plugins/rpsplug_createcommutativeoperator.cc
+++ plugins/rpsplug_createcommutativeoperator.cc
@@ -8,7 +8,7 @@
  * ./refpersys --plugin-after-load=/tmp/rpsplug_createcommutativeoperator.so \
  *             --plugin-arg=rpsplug_createcommutativeoperator:++ \
  *             --extra=name=plusplus \
- *             --extra=priority=8 \
+ *             --extra=precedence=8 \
  *             --batch --dump=.                                                
  *
  ****/
@@ -19,18 +19,27 @@ void
 rps_do_plugin(const Rps_Plugin* plugin)
 {
   RPS_LOCALFRAME(/*descr:*/nullptr, /*callerframe:*/nullptr,
-                           Rps_ObjectRef obnewroot;
+		 Rps_ObjectRef obnewroot;
+		 Rps_ObjectRef obcommutclass;
+		 Rps_ObjectRef obclassoper;
+                 Rps_Value strname;
                 );
   const char*plugarg = rps_get_plugin_cstr_argument(plugin);
   const char*xtraname = rps_get_extra_arg("name");
-  const char*xtrapriority = rps_get_extra_arg("priority");
+  const char*xtraprecedence = rps_get_extra_arg("precedence");
+  /// get repl_precedence attribute, conventionally values are small non-negative tagged integers
+  _f.obreplprecedence = RPS_ROOT_OB(_7iVRsTR8u3D00Cy0hp); //repl_precedence∈symbol
+  /// get the repl_operator superclass
+  _f.obclassoper =  RPS_ROOT_OB(_9j12Nhm4itk00YYUW7); //repl_operator∈class
+  RPS_ASSERT(_f.obclassoper != nullptr);
   if (!plugarg)
     RPS_FATALOUT("failure: plugin " << plugin->plugin_name
                  << " without argument; should be some string");
   RPS_INFORMOUT("running plugin " << plugin->plugin_name << " with argument "
                 << Rps_QuotedC_String(plugarg)
 		<< " and extra name " << Rps_QuotedC_String(xtraname)
-		<< " and extra priority " << Rps_QuotedC_String(xtrapriority));
+		<< " and extra precedence " << Rps_QuotedC_String(xtraprecedence));
+  _f.obcommutclass = 
   RPS_FATALOUT("rpsplug_createcommutativeoperator not implemented for "
                <<  Rps_QuotedC_String(plugarg));
 #warning unimplemented rpsplug_createcommutativeoperator
