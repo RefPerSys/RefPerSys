e07c479bf628a4740a074f761c1fe06ea5a8fc7c
Basile Starynkevitch <basile@starynkevitch.net>
2022-12-31 10:27:30 +0100

    plugins/rpsplug_createcommutativeoperator.cc still incomplete

--- plugins/rpsplug_createcommutativeoperator.cc
+++ plugins/rpsplug_createcommutativeoperator.cc
@@ -9,7 +9,7 @@
  *             --plugin-arg=rpsplug_createcommutativeoperator:++ \
  *             --extra=name=plusplus \
  *             --extra=precedence=8 \
- *             --batch --dump=.                                                
+ *             --batch --dump=.
  *
  ****/
 
@@ -19,11 +19,18 @@ void
 rps_do_plugin(const Rps_Plugin* plugin)
 {
   RPS_LOCALFRAME(/*descr:*/nullptr, /*callerframe:*/nullptr,
-		 Rps_ObjectRef obnewroot;
-		 Rps_ObjectRef obcommutclass;
-		 Rps_ObjectRef obclassoper;
-                 Rps_Value strname;
+                           Rps_ObjectRef obnewroot;
+                           Rps_ObjectRef obclasscommut;
+                           Rps_ObjectRef obclassoper;
+                           Rps_ObjectRef obclassrepldelim;
+                           Rps_ObjectRef obreplprecedence;
+                           Rps_Value strname;
                 );
+#define MYARGMAXLEN 64
+  char argcopy[MYARGMAXLEN];
+  bool argispunct = false;
+  bool argisident = false;
+  memset (argcopy, 0, MYARGMAXLEN);
   const char*plugarg = rps_get_plugin_cstr_argument(plugin);
   const char*xtraname = rps_get_extra_arg("name");
   const char*xtraprecedence = rps_get_extra_arg("precedence");
@@ -31,15 +38,46 @@ rps_do_plugin(const Rps_Plugin* plugin)
   _f.obreplprecedence = RPS_ROOT_OB(_7iVRsTR8u3D00Cy0hp); //repl_precedence∈symbol
   /// get the repl_operator superclass
   _f.obclassoper =  RPS_ROOT_OB(_9j12Nhm4itk00YYUW7); //repl_operator∈class
-  RPS_ASSERT(_f.obclassoper != nullptr);
-  if (!plugarg)
+  _f.obclasscommut =  RPS_ROOT_OB(_2dvQOlSMjOu02zWx1n); //repl_commutative_operator_class∈class
+  _f.obclassrepldelim = RPS_ROOT_OB(_2wdmxJecnFZ02VGGFK); //repl_delimiter∈class
+  RPS_ASSERT(_f.obclassoper);
+  RPS_ASSERT(_f.obclasscommut);
+  RPS_ASSERT(_f.obclassrepldelim);
+  RPS_ASSERT(_f.obclassoper->is_class());
+  RPS_ASSERT(_f.obclasscommut->is_class());
+  RPS_ASSERT(_f.obclassrepldelim->is_class());
+  if (!plugarg || plugarg[0]==(char)0)
     RPS_FATALOUT("failure: plugin " << plugin->plugin_name
-                 << " without argument; should be some string");
+                 << " without argument; should be some non-empty string");
+  if (strlen(plugarg) >= MYARGMAXLEN-1)
+    RPS_FATALOUT("failure: plugin " << plugin->plugin_name
+                 << " with too long argument " << Rps_QuotedC_String(plugarg));
+  strncpy(argcopy, plugarg, MYARGMAXLEN);
+  if (ispunct(plugarg[0]))
+    {
+      bool allpunct = true;
+      for (const char*pc = plugarg; allpunct && *pc; pc++)
+        allpunct = ispunct(*pc);
+      argispunct = allpunct;
+    };
+  if (isalpha(plugarg[0]))
+    {
+      bool allident = true;
+      for (const char*pc = plugarg; allident && *pc; pc++)
+        allident = isalnum(*pc) || *pc=='_';
+      argisident = allident;
+    }
+  if (!argispunct && !argisident)
+    RPS_FATALOUT("rpsplug_createcommutativeoperator with bad argument "
+                 <<  Rps_QuotedC_String(plugarg)
+                 << " not identifier or all-delim");
   RPS_INFORMOUT("running plugin " << plugin->plugin_name << " with argument "
                 << Rps_QuotedC_String(plugarg)
-		<< " and extra name " << Rps_QuotedC_String(xtraname)
-		<< " and extra precedence " << Rps_QuotedC_String(xtraprecedence));
-  _f.obcommutclass = 
+                << " and extra name " << Rps_QuotedC_String(xtraname)
+                << " and extra precedence " << Rps_QuotedC_String(xtraprecedence));
+  /** TODO:
+   *
+   **/
   RPS_FATALOUT("rpsplug_createcommutativeoperator not implemented for "
                <<  Rps_QuotedC_String(plugarg));
 #warning unimplemented rpsplug_createcommutativeoperator
