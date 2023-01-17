841d5888bfae94068918848aff7b8869a818e596
Basile Starynkevitch <basile@starynkevitch.net>
2023-01-17 21:55:45 +0100

    missing code in plugins/rpsplug_createclass.cc

--- plugins/rpsplug_createclass.cc
+++ plugins/rpsplug_createclass.cc
@@ -5,10 +5,9 @@
 // This plugin installs a commutative REPL operator
 /*****
  * Once compiled, use it for example as:
- * ./refpersys --plugin-after-load=/tmp/rpsplug_createcommutativeoperator.so \
- *             --plugin-arg=rpsplug_createcommutativeoperator:++ \
- *             --extra=name=plusplus \
- *             --extra=precedence=8 \
+ * ./refpersys --plugin-after-load=/tmp/rpsplug_createclass.so \
+ *             --plugin-arg=rpsplug_createclass:new_class_name \
+ *             --extra=super=superclass \
  *             --batch --dump=.
  *
  ****/
@@ -19,17 +18,17 @@ void
 rps_do_plugin(const Rps_Plugin* plugin)
 {
   RPS_LOCALFRAME(/*descr:*/nullptr, /*callerframe:*/nullptr,
-		 Rps_ObjectRef obsuperclass,
-		 Rps_ObjectRef obnewclass,
-		 Rps_Value classname; // a string
-		 );
+                           Rps_ObjectRef obsuperclass,
+                           Rps_ObjectRef obnewclass,
+                           Rps_Value classname; // a string
+                );
   const char*plugarg = rps_get_plugin_cstr_argument(plugin);
   const char*supername = rps_get_extra_arg("super");
   if (!plugarg || plugarg[0]==(char)0)
     RPS_FATALOUT("failure: plugin " << plugin->plugin_name
                  << " without argument; should be some non-empty name");
   /* TODO: check that supername is given and is naming an existing class. */
-  /// FIXME: missing code checking supername and computing obsuperclass. 
+  /// FIXME: missing code checking supername and computing obsuperclass.
   ///
   /* TODO: check that plugarg is a proper class name. */
   /// FIXME: check that plugarg is a proper class name.
