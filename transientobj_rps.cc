818316dce69b64669e29d8c6fdb0718db1098cff
Basile Starynkevitch <basile@starynkevitch.net>
2023-02-12 10:33:56 +0100

    start adding transientobj_rps.cc ...

---
+++ transientobj_rps.cc
@@ -0,0 +1,45 @@
+/****************************************************************
+ * file transientobj_rps.cc
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Description:
+ *      This file is part of the Reflective Persistent System.
+ *
+ *      Low-level implementation of transient objects and payloads
+ *
+ * Author(s):
+ *      Basile Starynkevitch <basile@starynkevitch.net>
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *      Nimesh Neema <nimeshneema@gmail.com>
+ *
+ *      © Copyright 2023 The Reflective Persistent System Team
+ *      team@refpersys.org & http://refpersys.org/
+ *
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+
+#include "refpersys.hh"
+
+extern "C" const char rps_transientobj_gitid[];
+const char rps_transientobj_gitid[]= RPS_GITID;
+
+extern "C" const char rps_transientobj_date[];
+const char rps_transientobj_date[]= __DATE__;
+
+
+#warning transientobj_rps.cc is nearly empty
+
+/*** end of file transientobj_rps.cc ***/
