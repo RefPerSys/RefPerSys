9af54fc87cc3a253d1b9d6d03051bc454a52fb30
Basile Starynkevitch <basile@starynkevitch.net>
2023-07-10 14:49:14 +0200

    more debug in incomplete Rps_TokenSource::parse_comparand

--- parsrepl_rps.cc
+++ parsrepl_rps.cc
@@ -1718,7 +1718,9 @@ Rps_TokenSource::parse_comparand(Rps_CallFrame*callframe, bool*pokparse)
                 << Rps_Do_Output([&](std::ostream& out)
   {
     this->display_current_line_with_cursor(out);
-  }));
+  })
+		<< std::endl
+		<< RPS_FULL_BACKTRACE_HERE(1, "incomplete Rps_TokenSource::parse_comparand"));
 #warning TODO: missing code in Rps_TokenSource::parse_comparand
   /***
    * We probably should loop and collect all terms if they are
