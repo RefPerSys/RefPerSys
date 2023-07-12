0fcf60b9118a66d97e87a8a8750d972aed557b9b
Basile Starynkevitch <basile@starynkevitch.net>
2023-07-12 09:14:00 +0200

    adding stub files for code generation: cppgen_rps.cc (for generation of C++ code) and lightgen_rps.cc (for generation with GNU lightning)

---
+++ cppgen_rps.cc
@@ -0,0 +1,47 @@
+/****************************************************************
+ * file cppgen_rps.cc
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Description:
+ *      This file is part of the Reflective Persistent System.
+ *
+ *      It has the code for machine code generation using GNU
+ *      lightning.  See also https://www.gnu.org/software/lightning/
+ *
+ * Author(s):
+ *      Basile Starynkevitch <basile@starynkevitch.net>
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *      Nimesh Neema <nimeshneema@gmail.com>
+ *
+ *      © Copyright 2023 - 2023 The Reflective Persistent System Team
+ *      team@refpersys.org & http://refpersys.org/
+ *
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Notice:
+ *    See also companion file lightgen_rps.cc for GNU lightning code generation.
+ ******************************************************************************/
+
+#include "refpersys.hh"
+
+
+
+extern "C" const char rps_lightgen_gitid[];
+const char rps_lightgen_gitid[]= RPS_GITID;
+
+extern "C" const char rps_lightgen_date[];
+const char rps_lightgen_date[]= __DATE__;
+
+#warning incomplete lightgen_rps.cc file
