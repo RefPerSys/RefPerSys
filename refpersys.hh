33ad30017f9a6d6ecdd0314702f4cb59d3703767
Basile Starynkevitch <basile@starynkevitch.net>
2023-07-13 19:04:07 +0200

    improve license phrasing

--- refpersys.hh
+++ refpersys.hh
@@ -33,7 +33,8 @@
  *    This program is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ *    GNU General Public License for more details or the Lesser
+ *    General Public License.
  *
  *    You should have received a copy of the GNU General Public License
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
