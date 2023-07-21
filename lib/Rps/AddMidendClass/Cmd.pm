c9564bff0607a3b52d174e4907fa36eaaadce9ce
Abhishek Chakravarti <ack@ctetra.net>
2023-07-21 12:51:11 +0530

    more comments on required packages
    Signed-off-by: Abhishek Chakravarti <ack@ctetra.net>

--- lib/Rps/AddMidendClass/Cmd.pm
+++ lib/Rps/AddMidendClass/Cmd.pm
@@ -1,1 +1,15 @@
 # Command line parsing
+# Required deb packages:
+# 1. libgetopt-long-descriptive-perl
+# 2. libsmart-comments-perl
+
+
+use 5.36.0;
+use Smart::Comments;
+use Getopt::Long;
+
+package Rps::AddMidendClass::Cmd;
+use Exporter 'import';
+
+
+1;
