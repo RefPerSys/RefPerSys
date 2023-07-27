f97443366458d32d0a1708dfe08c8389f5b39c58
Basile Starynkevitch <basile@starynkevitch.net>
2023-07-27 18:06:44 +0200

    rps_parse_program_arguments has its aix argument index for argp_parse

--- utilities_rps.cc
+++ utilities_rps.cc
@@ -1032,8 +1032,10 @@ rps_parse_program_arguments(int &argc, char**argv)
   argparser_rps.children = nullptr;
   argparser_rps.help_filter = nullptr;
   argparser_rps.argp_domain = nullptr;
-  if (argp_parse(&argparser_rps, argc, argv, 0, nullptr, nullptr))
-    RPS_FATALOUT("failed to parse program arguments to " << argv[0]);
+  int aix= -1;
+  if (argp_parse(&argparser_rps, argc, argv, 0, &aix, nullptr))
+    RPS_FATALOUT("failed to parse program arguments to " << argv[0]
+		 << " at program argument index aix=" << aix);
   RPS_POSSIBLE_BREAKPOINT();
 } // end rps_parse_program_arguments
 
