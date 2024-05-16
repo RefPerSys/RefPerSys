8d7ccdabae27389155f73a8cda9a91373e8415ba
Basile Starynkevitch <basile@starynkevitch.net>
2024-05-16 13:16:46 +0200

    rps_show_version showing rps_fltk_abi_version & rps_fltk_api_version

--- utilities_rps.cc
+++ utilities_rps.cc
@@ -550,6 +550,10 @@ rps_show_version(void)
             << " executable: " << exepath;
   if (strcmp(exepath, realexepath))
     std::cout <<  " really " << realexepath;
+  std::cout << " FLTK (see fltk.org) ABI version:" << rps_fltk_abi_version()
+            << std::endl;
+  std::cout << " FLTK API version:" << rps_fltk_api_version()
+            << std::endl;
   std::cout << std::endl
             /* TODO: near commit 191d55e1b31c, march 2023; decide
                which parser generator to really use... and drop the
