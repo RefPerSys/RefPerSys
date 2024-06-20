5c78fb8479e6674f425fe5237a58fe6586e28a61
Basile Starynkevitch <basile@starynkevitch.net>
2024-06-20 16:48:40 +0200

    main is showing final debug flags
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- main_rps.cc
+++ main_rps.cc
@@ -1546,14 +1546,14 @@ main (int argc, char** argv)
                 << " loaded state " << rps_my_load_dir << std::endl
                 << " elapsed " << rps_elapsed_real_time()
                 << ", cpu " << rps_process_cpu_time() << " seconds;" << std::endl
-		<< "Final debug flags:" 
+                << "Final debug flags:"
                 << Rps_Do_Output([&](std::ostream& out)
-		{
-		  rps_output_debug_flags(out);
-		}) << std::endl
-                << " invocation was:" << std::endl
-                << RPS_OUT_PROGARGS(argc, argv) << std::endl
-                << Rps_Do_Output([&](std::ostream& out)
+  {
+    rps_output_debug_flags(out);
+  }) << std::endl
+     << " invocation was:" << std::endl
+     << RPS_OUT_PROGARGS(argc, argv) << std::endl
+     << Rps_Do_Output([&](std::ostream& out)
   {
     int ex = rps_exit_atomic_code.load();
     if (ex==0)
