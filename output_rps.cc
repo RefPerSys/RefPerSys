766057ed05176855f2160bafdfaa932e068daaf1
Basile Starynkevitch <basile@starynkevitch.net>
2024-09-23 18:31:44 +0200

    drafted output_rps.cc file, hoping for someone to move Rps_OutputValue code here...
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

---
+++ output_rps.cc
@@ -0,0 +1,45 @@
+/****************************************************************
+ * file output_rps.cc
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Description:
+ *      This file is part of the Reflective Persistent System.
+ *      Implementation related to pretty output of values and objects
+ *
+ * Author(s):
+ *      Basile Starynkevitch <basile@starynkevitch.net>
+ *      Abhishek Chakravarti <abhishek@taranjali.org>
+ *      Nimesh Neema <nimeshneema@gmail.com>
+ *
+ *      © Copyright 2024 - 2024 The Reflective Persistent System Team
+ *      team@refpersys.org & http://refpersys.org/
+ *
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include "refpersys.hh"
+
+extern "C" const char rps_output_gitid[];
+const char rps_output_gitid[]= RPS_GITID;
+
+extern "C" const char rps_output_date[];
+const char rps_output_date[]= __DATE__;
+
+extern "C" const char rps_output_shortgitid[];
+const char rps_output_shortgitid[]= RPS_SHORTGITID;
+
+////////////////
+
+#warning TODO: add or move code related to pretty output here
