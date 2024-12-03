8385cd138843565267737568b0e848980e783395
Abhishek Chakravarti <ack@fifthestate.co.in>
2024-12-03 12:26:32 +0530

    better SPDX header comment
    Signed-off-by: Abhishek Chakravarti <ack@fifthestate.co.in>

--- rps-generate-gitid.sh
+++ rps-generate-gitid.sh
@@ -1,33 +1,27 @@
 #!/bin/sh
 
-#% SPDX-License-Identifier: GPL-3.0-or-later
-# RefPerSys file rps-generate-gitid.sh - see refpersys.org
-###
+# Copyright 2020 - 2024, Basile Starynkevitch and the forum@refpersys.org
+# mailing list contributors
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#
+# This file is part of the Reflexive Persistent System (aka RefPerSys);
 # it just emits a string with the full git commit id, appending + if
 # the git status is not clean.
-#%
-#%      © Copyright 2020-2022 The Reflective Persistent System Team
-#%      <http://refpersys.org>
-#%
-#% Description:
-#%
-#%      This file is part of the Reflective Persistent System.
-#%      This script is used by the Makefile and emits a string with the gitid
-#%
-#%
-#% License:
-#%    This program is free software: you can redistribute it and/or modify
-#%    it under the terms of the GNU General Public License as published by
-#%    the Free Software Foundation, either version 3 of the License, or
-#%    (at your option) any later version.
-#%
-#%    This program is distributed in the hope that it will be useful,
-#%    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#%    GNU General Public License for more details.
-#%
-#%    You should have received a copy of the GNU General Public License
-#%    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 license()
 {
