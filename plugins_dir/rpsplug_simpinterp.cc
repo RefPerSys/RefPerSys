14e9d75b892811b8e05af0ac05b7b14dcedc3732
Basile Starynkevitch <basile@starynkevitch.net>
2025-03-07 09:49:20 +0100

    plugins_dir/rpsplug_simpinterp.cc uses Rps_MemoryFileTokenSource
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- plugins_dir/rpsplug_simpinterp.cc
+++ plugins_dir/rpsplug_simpinterp.cc
@@ -21,36 +21,10 @@
 
 #include "refpersys.hh"
 
-extern "C" char *rpsint_start;
-extern "C" char *rpsint_cur;
-extern "C" char *rpsint_end;
-extern "C" int rpsint_lineno;
-
 
 /// on whatsapp March 14 2024 Abhishek approved a C like syntax; we
 /// hope for him to be allowed to informally specify it in Feb 2025
 // See https://github.com/RefPerSys/RefPerSys/issues/21
-
-#warning TODO: rpsint_parse_script has to return values which would be interpreted by future rpsint_eval_script
-extern "C" void rpsint_parse_script(Rps_CallFrame*cf, Rps_ObjectRef ob);
-extern "C" void rpsint_skip_spaces(void);
-extern "C" bool rpsint_has_keyword(const char*kw);
-extern "C" bool rpsint_has_intp(intptr_t* ii);
-extern "C" bool rpsint_has_doublep(double* pd);
-extern "C" bool rpsint_has_refint(intptr_t& r);
-extern "C" bool rpsint_has_refdouble(double& r);
-
-bool
-rpsint_has_refint(intptr_t& r)
-{
-  return rpsint_has_intp(&r);
-} // end rpsint_has_refint
-
-bool
-rpsint_has_refdouble(double& r)
-{
-  return rpsint_has_doublep(&r);
-} // end rpsint_has_refdouble
 
 void
 rps_do_plugin(const Rps_Plugin*plugin)
@@ -59,8 +33,6 @@ rps_do_plugin(const Rps_Plugin*plugin)
                            Rps_ObjectRef ob;
                            Rps_Value v1;
                 );
-  int file_fd = -1;
-  size_t file_len = 0;
   _f.ob = nullptr;
   _f.v1 = nullptr;
   errno = 0;
@@ -81,119 +53,19 @@ rps_do_plugin(const Rps_Plugin*plugin)
       RPS_FATALOUT("failure: plugin "  << plugin->plugin_name
                    << " cannot stat script argument " << plugarg
                    << ":" << strerror(errno));
-    if ((plugstat.st_mode & S_IFMT) != S_IFREG)
-      RPS_FATALOUT("failure: plugin " <<  plugin->plugin_name
-                   << " script argument " << plugarg << " not a regular file");
-    file_len = plugstat.st_size;
-  }
-  /// map the file
-  file_fd = open(plugarg, O_RDONLY | O_CLOEXEC);
-  if (file_fd < 0)
-    {
-      RPS_FATALOUT("failure: plugin " <<  plugin->plugin_name
-                   << " failed to open " << plugarg
-                   << ":" << strerror(errno));
+    if ((plugstat.st_mode & S_IFMT) != S_IFREG) {
+      RPS_WARNOUT("failure: plugin " <<  plugin->plugin_name
+                   << " script argument " << plugarg
+		  << " not a regular file" << std::endl
+		  << RPS_FULL_BACKTRACE_HERE(1, "simple interpreter"));
+      return;
     };
-  {
-    void*ad = mmap(nullptr, file_len, PROT_READ, MAP_SHARED, file_fd, (off_t)0);
-    if (ad == MAP_FAILED)
-      RPS_FATALOUT("failure: plugin " <<  plugin->plugin_name
-                   << " failed to mmap " << plugarg
-                   << ":" << strerror(errno));
-    rpsint_start = (char*)ad;
-    rpsint_cur = rpsint_start;
-    rpsint_end = (char*)ad + file_len;
-  };
-  _f.ob = Rps_PayloadObjMap::make(&_);
-  rpsint_lineno = 1;
-  rpsint_parse_script(&_, _f.ob);
+  }
+  Rps_MemoryFileTokenSource toksrc(plugarg);
   RPS_WARNOUT("missing code:  plugin " <<  plugin->plugin_name
-              << " script " << plugarg << " of " << file_len << " bytes");
+              << " script " << plugarg);
 #warning a lot of missing code in rpsplug_simpinterp.cc
 } // end rps_do_plugin
-
-void
-rpsint_skip_spaces(void)
-{
-  while (rpsint_cur < rpsint_end)
-    {
-      if (!isspace(*rpsint_cur)) return;
-      if (*rpsint_cur=='\n') rpsint_lineno++;
-      rpsint_cur++;
-    }
-} // end rpsint_skip_spaces
-
-void
-rpsint_parse_script(Rps_CallFrame*cf, Rps_ObjectRef obint)
-{
-  RPS_LOCALFRAME(/*descr:*/nullptr, /*callerframe:*/cf,
-                           Rps_ObjectRef obint;
-                );
-  _f.obint = obint;
-  rpsint_skip_spaces();
-#warning empty rpsint_parse_script
-  RPS_WARNOUT("empty rpsint_parse_script obint=" << _f.obint);
-} // end rpsint_parse_script
-
-bool
-rpsint_has_keyword(const char*kw)
-{
-  RPS_ASSERT(kw && isalpha(kw[0]));
-  rpsint_skip_spaces();
-  int kwlen = strlen(kw);
-  if (rpsint_cur+kwlen>rpsint_end)
-    return false;
-  for (int i=0; i<kwlen; i++)
-    if (kw[i] != rpsint_cur[i])
-      return false;
-  if (!isalnum(rpsint_cur[kwlen]) || rpsint_cur[kwlen]=='_')
-    return false;
-  rpsint_cur += kwlen;
-  return true;
-} // end rpsint_has_keyword
-
-bool
-rpsint_has_intp(intptr_t* pi)
-{
-  if (!pi) return false;
-  rpsint_skip_spaces();
-  if (rpsint_cur>rpsint_end)
-    return false;
-  long l= 0;
-  char*end=nullptr;
-  l = strtol(rpsint_cur,&end,0);
-  if (end>rpsint_cur)
-    {
-      rpsint_cur += (end-rpsint_cur);
-      *pi = (intptr_t)l;
-      return true;
-    };
-  return false;
-} // end rpsint_has_intp
-
-bool
-rpsint_has_doublep(double* pd)
-{
-  if (!pd) return false;
-  rpsint_skip_spaces();
-  if (rpsint_cur>rpsint_end)
-    return false;
-  double d= 0;
-  char*end=nullptr;
-  d = strtod(rpsint_cur,&end);
-  if (end>rpsint_cur)
-    {
-      rpsint_cur += (end-rpsint_cur);
-      *pd = d;
-      return true;
-    };
-  return false;
-} // end rpsint_has_doublep
-
-char *rpsint_start;
-char *rpsint_cur;
-char *rpsint_end;
-int rpsint_lineno;
 
 /****************
  **                           for Emacs...
