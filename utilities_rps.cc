8e58a0e426c0535901efa058eaaf33745aa06f61
Basile Starynkevitch <basile@starynkevitch.net>
2025-03-31 15:46:59 +0200

    skipping some suffixes in rps_show_version_handlwritten_source_files
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- utilities_rps.cc
+++ utilities_rps.cc
@@ -568,6 +568,8 @@ rps_show_version_handwritten_source_files(void)
     {
       char curbase[64];
       memset (curbase, 0, sizeof(curbase));
+      char cursuffix[16];
+      memset (cursuffix, 0, sizeof(cursuffix));
       int endpos = -1;
       const char*curfile = *curfileptr;
       if (!curfile)
@@ -578,9 +580,17 @@ rps_show_version_handwritten_source_files(void)
         continue;
       if (strchr(curfile, '/'))
         continue;
-      if ((sscanf(curfile, "%60[a-zA-Z]_rps.cc%n", curbase, &endpos))<1
+      if ((sscanf(curfile, "%60[a-zA-Z]_rps.%.10[a-z]%n", curbase, cursuffix, &endpos))<1
           || endpos<2 || curfile[endpos]!=(char)0)
         continue;
+      if (!strcmp(cursuffix, "so") || !strcmp(cursuffix, "o") || !strcmp(cursuffix, "a")
+          || !strcmp(cursuffix, "la") || !strcmp(cursuffix, "status"))
+        {
+          RPS_POSSIBLE_BREAKPOINT();
+          RPS_DEBUG_LOG(PROGARG, "curfile=" << Rps_Cjson_String(curfile)
+                        << " skipped cursuffix=" << Rps_Cjson_String(cursuffix));
+          continue;
+        };
       const char* symgit = nullptr;
       const char* symdat = nullptr;
       const char* symshortgit = nullptr;
