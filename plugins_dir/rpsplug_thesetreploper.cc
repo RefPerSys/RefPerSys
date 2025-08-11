a3098a8e8f3d53be19c8a07a2fc2d2c2e8f1db74
Basile Starynkevitch <basile@starynkevitch.net>
2025-08-11 17:29:04 +0200

    more code in plugins_dir/rpsplug_thesetreploper.cc

--- plugins_dir/rpsplug_thesetreploper.cc
+++ plugins_dir/rpsplug_thesetreploper.cc
@@ -16,6 +16,7 @@ rps_do_plugin(const Rps_Plugin* plugin)
 {
   RPS_LOCALFRAME(/*descr:*/nullptr, /*callerframe:*/nullptr,
                            Rps_ObjectRef obnewsetoper;
+                           Rps_ObjectRef obsymbol;
                            Rps_Value strname;
                 );
 #define MYARGMAXLEN 64
@@ -23,10 +24,25 @@ rps_do_plugin(const Rps_Plugin* plugin)
   bool argispunct = false;
   bool argisident = false;
   int precedence = -1;
+  constexpr const char* nm = "set_of_repl_operators";
   memset (argcopy, 0, MYARGMAXLEN);
   const char*plugarg = rps_get_plugin_cstr_argument(plugin);
 #warning incomplete rpsplug_thesetreploper.cc
-
+  _f.obnewsetoper =
+    Rps_PayloadSetOb::make_mutable_set_object(&_,
+					      RPS_ROOTOB(_0J1C39JoZiv03qA2HA), // mutable_set
+					      Rps_ObjectRef::root_space());
+  std::lock_guard<std::recursive_mutex> gunewsetoper(*(_f.obnewsetoper->objmtxptr()));
+  _f.strname = Rps_StringValue(nm);
+  _f.obsymbol = Rps_ObjectRef::make_new_strong_symbol(&_, nm);
+  std::lock_guard<std::recursive_mutex> guobsymbol(*(_f.obsymbol->objmtxptr()));
+  Rps_PayloadSymbol* paysymb = _f.obsymbol->get_dynamic_payload<Rps_PayloadSymbol>();
+  RPS_ASSERT(paysymb != nullptr);
+  _f.obnewsetoper->put_attr(RPS_ROOT_OB(_1EBVGSfW2m200z18rx), //name∈named_attribute
+			    _f.strname);
+  _f.obnewsetoper->put_attr(RPS_ROOT_OB(_3Q3hJsSgCDN03GTYW5), //symbol∈symbol
+			    _f.obsymbol);
+  paysymb->symbol_put_value(_f.obnewsetoper);
   /** TODO: We need to create a single constant object, named
       set_of_repl_operators, whose payload is a mutable set of
       objects.  Once dumped successfully, we need to improve the
