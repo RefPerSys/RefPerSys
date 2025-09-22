c5f35ad3947bf0955a2b73aeb4fa5c686ef1d8f2
Basile Starynkevitch <basile@starynkevitch.net>
2025-09-22 19:59:46 +0200

    improve test_dir/005script.bash
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- test_dir/005script.bash
+++ test_dir/005script.bash
@@ -3,7 +3,7 @@ if [ -x refpersys ]; then
     echo 'no refpersys executable in ' $(/bin/pwd) > /dev/stderr
     exit 1
 fi
-./refpersys -AREPL --script=$0 --batch 
+./refpersys -AREPL --script=$0 --batch --run-name=$0
 
 ## magic string REFPERSYS_SCRIPT foo
 ## etc
