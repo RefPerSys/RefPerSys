c8c73b699c7c2bdf36cced6f9e643c82d20cd52e
Basile Starynkevitch <basile@starynkevitch.net>
2025-09-22 16:57:56 +0200

    added test_dir/005script.bash
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

---
+++ test_dir/005script.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -x refpersys ]; then
+    echo 'no refpersys executable in ' $(/bin/pwd) > /dev/stderr
+    exit 1
+fi
+./refpersys -AREPL --script=$0 --batch 
+
+## magic string REFPERSYS_SCRIPT foo
+## etc
