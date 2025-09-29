3909c90154f4e87bbc20270ce4b6da3fd3c116be
Basile Starynkevitch <basile@starynkevitch.net>
2025-09-29 12:04:27 +0200

    test_dir/005script.bash had a typo but is still failing
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- test_dir/005script.bash
+++ test_dir/005script.bash
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-if [ -x refpersys ]; then
+if [ ! -x refpersys ]; then
     echo 'no refpersys executable in ' $(/bin/pwd) > /dev/stderr
     exit 1
 fi
