63d2a95ea6d13e75bebf0ad29919631e805dc81b
Basile Starynkevitch <basile@starynkevitch.net>
2025-10-03 12:11:59 +0200

    improve plugins_dir/rpsplug_cplusplustypes.cc
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- plugins_dir/rpsplug_cplusplustypes.cc
+++ plugins_dir/rpsplug_cplusplustypes.cc
@@ -14,7 +14,7 @@
 #include "refpersys.hh"
 
 void
-rpscplusplustype(Rps_CallFrame*callframe, const char*obid, const char*cppname)
+rpscplusplustype(Rps_CallFrame*callframe, const char*obid, const char*cppname, int size, int align)
 {
   RPS_LOCALFRAME(/*descr:*/nullptr, /*callerframe:*/callframe,
 		 Rps_ObjectRef obty;
@@ -23,12 +23,22 @@ rpscplusplustype(Rps_CallFrame*callframe, const char*obid, const char*cppname)
   _f.obty = Rps_ObjectRef::find_object_or_fail_by_string(&_, obid);
   _f.str = Rps_Value(std::string(cppname));
   RPS_ASSERT(_f.obty->is_instance_of(RPS_ROOT_OB(_1XswYkom3Jm02YR3Vi))); /*cplusplus_primitive_type∈class*/;
-  if (_f.obty->get_physical_attr(rpskob_0fx0GtCX90Z03VI9mo /*!cplusplus_name∈named_attribute*/	))
+  if (_f.obty->get_physical_attr(rpskob_0fx0GtCX90Z03VI9mo /*!cplusplus_name∈named_attribute*/	)) {
     RPS_WARNOUT("already filled obty: " << RPS_OBJECT_DISPLAY(_f.obty));
+    return;
+  }
   else 
     _f.obty							
       ->put_attr(rpskob_0fx0GtCX90Z03VI9mo, /*!cplusplus_name∈named_attribute*/	
-		 _f.str);						
+		 _f.str);
+  if (size > 0) 
+    _f.obty							
+      ->put_attr(rpskob_8IRzlYX53kN00tC3fG, //!byte_size∈named_attribute 
+		 Rps_Value::make_tagged_int(size));
+  if (align > 0)
+    _f.obty							
+      ->put_attr(rpskob_6EsfxShTuwH02waeLE, //!byte_alignment∈named_attribute
+		 Rps_Value::make_tagged_int(align));
   RPS_INFORMOUT("updated" << std::endl << RPS_OBJECT_DISPLAY(_f.obty) 
 		<< std::endl);
 } // end rpscplusplustype
@@ -45,7 +55,8 @@ rps_do_plugin(const Rps_Plugin* plugin)
   RPS_DEBUG_LOG(CMD, "start plugin "
                       << plugin->plugin_name << " from " << std::endl
                 << RPS_FULL_BACKTRACE_HERE(1, "C++ types plugin"));
-#define RPS_TYPE_CPLUSPLUS(Id,Cstr) rpscplusplustype(&_, #Id, #Cstr)
+#define RPS_TYPE_CPLUSPLUS(Id,Cnam) \
+    rpscplusplustype(&_, #Id, #Cnam, sizeof(Cnam), alignof(Cnam))
   RPS_TYPE_CPLUSPLUS(_4V1oeUOvmxo041XLTm,intptr_t);
   RPS_TYPE_CPLUSPLUS(_4nZ0jIKUbGr01OixPV,int);
   RPS_TYPE_CPLUSPLUS(_3NYlqvmSuTm024LDuD,long);
