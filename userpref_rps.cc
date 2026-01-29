6fb0d152a6e0db348f0b099e1e0d3ea1789a6c91
Basile Starynkevitch <basile@starynkevitch.net>
2026-01-29 14:14:33 +0100

    we have the rps_userpath_path
    Signed-off-by: Basile Starynkevitch <basile@starynkevitch.net>

--- userpref_rps.cc
+++ userpref_rps.cc
@@ -80,26 +80,32 @@ rps_delete_user_preferences(void)
   rps_userpref_ird = nullptr;
 } // end rps_delete_user_preferences
 
+static void
+rps_free_userpref(void*p1, void*p2 [[unused]])
+{
+  free(p1);
+} // end rps_free_userpref
+
 void
 rps_set_user_preferences(char*path)
 {
   RPS_POSSIBLE_BREAKPOINT();
   /// to set manually the REPL debugging use rps_add_debug_cstr("REPL") in GDB
-  if (!path || !path[0] || !strcmp(path, ".") || !strcmp(path, "/")) {
-    RPS_INFORMOUT("skipping user preferences given by "
-		  <<  Rps_QuotedC_String(path) << std::endl
-		  << RPS_FULL_BACKTRACE(1, "rps_set_user_preferences/skip"));
-    rps_userpref_path = nullptr;
-    return;
-  };
+  if (!path || !path[0] || !strcmp(path, ".") || !strcmp(path, "/"))
+    {
+      RPS_INFORMOUT("skipping user preferences given by "
+                    <<  Rps_QuotedC_String(path) << std::endl
+                    << RPS_FULL_BACKTRACE(1, "rps_set_user_preferences/skip"));
+      rps_userpref_path = nullptr;
+      return;
+    };
   RPS_ASSERT(!access(path, R_OK));
   rps_userpref_path = strdup(path);
   if (!rps_userpref_path)
     RPS_FATALOUT("failed to strdup " << path);
-  rps_do_at_exit_cpp([&](void) {
-    free (rps_userpref_path);
-    rps_userpref_path = nullptr;
-  };
+  rps_do_at_exit_cfun(rps_free_userpref,
+                      (void*)(rps_userpref_path),
+                      nullptr);
   RPS_ASSERT(rps_is_main_thread());
   if (rps_userpref_mts)
     RPS_FATALOUT("cannot set user preferences more than once, here to "
@@ -422,7 +428,7 @@ rps_try_parsing_default_user_preferences(void)
           rps_set_user_preferences(buf);
         }
       else
-	RPS_WARNOUT("no default user preference file " << buf);
+        RPS_WARNOUT("no default user preference file " << buf);
     }
 } // end rps_try_parsing_default_user_preferences
 
