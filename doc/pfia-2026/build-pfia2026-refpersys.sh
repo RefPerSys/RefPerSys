0196e2912510e0b112151291d4151021eb0568c4
Basile STARYNKEVITCH <basile@starynkevitch.net>
2026-04-17 18:10:35 +0200

    start adding doc/pfia-2026 for submission to https://pfia26.cril.fr/journ%C3%A9es/journee3/incertitude-ia/
    Signed-off-by: Basile STARYNKEVITCH <basile@starynkevitch.net>

---
+++ doc/pfia-2026/build-pfia2026-refpersys.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+# file RefPerSys/doc/pfia2026/build-pfia2026-refpersys.sh
+# Build a submission in French as PDF file to https://pfia26.cril.fr/
+# 
+# https://pfia26.cril.fr/journ%C3%A9es/journee3/incertitude-ia/ 
+echo $0 Starting in $(pwd)
+rm -rfv RefPerSys-PFIA-2026-Starynkevitch..{aux,bcf,blg,log,pdf} generated-pfia2026-gitid.tex
+
+printf "\n\n generating pfia2026-gitid\n"
+
+rawgittag="$(git log --format=oneline -1 --abbrev=16 --abbrev-commit -q|cut -d' ' -f1)"
+
+if git status -s | grep -q '^.M' > /dev/null ; then
+    gittag=$(printf "%s++" "$rawgittag")
+else
+    gittag=$(printf "%s..." "$rawgittag")
+fi
+printf "\\\\newcommand{\\\\rpsgitcommit}[0]{%s}\n" "$gittag" > generated-pfia2026.tex
+env LANG=fr_FR.UTF-8 date +"\\newcommand{\\rpsgitdate}[0]{%d %b %Y}\n" > generated-pfia2026.tex
+
+printf "\n\n ================ SVG processing by inkscape of "; echo *.svg "files."
+for svgfile in *.svg ; do
+    svgbase=$(basename $svgfile .svg)
+    inkscape -o $svgbase.pdf $svgfile
+    inkscape -o $svgbase.eps $svgfile
+done
+
