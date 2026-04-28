9f6a42c7e484f6a2710bbd31df1f13e51b8f9dc3
Basile STARYNKEVITCH <basile@starynkevitch.net>
2026-04-28 10:33:35 +0200

    minor improvements in plugins_dir/rpsplug_createmonoper.cc
    Signed-off-by: Basile STARYNKEVITCH <basile@starynkevitch.net>

--- plugins_dir/rpsplug_createmonoper.cc
+++ plugins_dir/rpsplug_createmonoper.cc
@@ -1,16 +1,17 @@
 // see http://refpersys.org/
 // passed to commits after 306c6f5f688a (in august 2025) of RefPerSys
 // GPLv3+ licensed
-// © Copyright 2025 Basile Starynkevitch <basile@starynkevitch.net>
+// © Copyright (C) 2025 - 2026 Basile Starynkevitch <basile@starynkevitch.net>
 // This plugin rpsplug_createmonoper.cc installs a monoadic unary REPL operator
 /*****
  * Once compiled, use it for example as:
- * ./refpersys --plugin-after-load=/tmp/rpsplug_createmonoper.so \
- *             --plugin-arg=rpsplug_createmonoper:- \
- *             --extra=name=negate \
- *             --extra=precedence=8 \
- *             --extra=comment='unary negation operator'
- *             --batch --dump=.
+ 
+    ./refpersys --plugin-after-load=/tmp/rpsplug_createmonoper.so \
+                --plugin-arg=rpsplug_createmonoper:- \
+                --extra=name=negate \
+                --extra=precedence=8 \
+                --extra=comment='unary negation operator'
+                --batch --dump=.
  *
  ****/
 
@@ -45,10 +46,11 @@ rps_do_plugin(const Rps_Plugin* plugin)
   const char*plugarg = rps_get_plugin_cstr_argument(plugin);
   if (!plugarg || plugarg[0]==(char)0)
     RPS_FATALOUT("failure: plugin " << plugin->plugin_name
-                 << " without argument; should be some non-empty string");
+                 << " without argument; should be a non-empty string");
   if (strlen(plugarg) >= MYARGMAXLEN-1)
     RPS_FATALOUT("failure: plugin " << plugin->plugin_name
-                 << " with too long argument " << Rps_QuotedC_String(plugarg));
+                 << " with too long argument "
+		 << Rps_QuotedC_String(plugarg));
   if (rooted)
     {
       if (!strcmp(rooted, "true"))
@@ -65,11 +67,14 @@ rps_do_plugin(const Rps_Plugin* plugin)
     };
   /// get repl_precedence attribute, conventionally values are small
   /// non-negative tagged integers
-  _f.obreplprecedence = RPS_ROOT_OB(_7iVRsTR8u3D00Cy0hp); //repl_precedence∈symbol
+  _f.obreplprecedence
+    = RPS_ROOT_OB(_7iVRsTR8u3D00Cy0hp); //repl_precedence∈symbol
   /// get the repl_operator superclass
-  _f.obclassoper =  RPS_ROOT_OB(_6vcJz35mTam01zYLjL); //repl_unary_operator∈class
+  _f.obclassoper
+    =  RPS_ROOT_OB(_6vcJz35mTam01zYLjL); //repl_unary_operator∈class
   RPS_ASSERT(_f.obclassoper->is_class());
-  _f.obclassrepldelim = RPS_ROOT_OB(_2wdmxJecnFZ02VGGFK); //repl_delimiter∈class
+  _f.obclassrepldelim
+    = RPS_ROOT_OB(_2wdmxJecnFZ02VGGFK); //repl_delimiter∈class
   RPS_ASSERT(_f.obclassoper);
   /** we might improve and later accept a subclass for the operator **/
   RPS_ASSERT(_f.obclassoper->is_class());
@@ -80,16 +85,18 @@ rps_do_plugin(const Rps_Plugin* plugin)
     precedence = atoi (xtraprecedence);
   else
     RPS_FATALOUT("failure: plugin " << plugin->plugin_name
-                 << " with bad precedence " << precedence << " for argument " << Rps_QuotedC_String(plugarg));
+                 << " with bad precedence " << precedence
+		 << " for argument " << Rps_QuotedC_String(plugarg));
 
   RPS_ASSERT(_f.obclassoper->is_class());
   RPS_ASSERT(_f.obclassrepldelim->is_class());
   if (!plugarg || plugarg[0]==(char)0)
     RPS_FATALOUT("failure: plugin " << plugin->plugin_name
-                 << " without argument; should be some non-empty string");
+                 << " without argument; should be a non-empty string");
   if (strlen(plugarg) >= MYARGMAXLEN-1)
     RPS_FATALOUT("failure: plugin " << plugin->plugin_name
-                 << " with too long argument " << Rps_QuotedC_String(plugarg));
+                 << " with too long argument "
+		 << Rps_QuotedC_String(plugarg));
   strncpy(argcopy, plugarg, MYARGMAXLEN);
   if (ispunct(plugarg[0]))
     {
@@ -109,21 +116,17 @@ rps_do_plugin(const Rps_Plugin* plugin)
     RPS_FATALOUT("rpsplug_createmonoper with bad argument "
                  <<  Rps_QuotedC_String(plugarg)
                  << " not identifier or all-delim");
-  RPS_INFORMOUT("running plugin " << plugin->plugin_name << " with argument "
+  RPS_INFORMOUT("running plugin " << plugin->plugin_name
+		<< " with argument "
                 << Rps_QuotedC_String(plugarg)
                 << " and extra name " << Rps_QuotedC_String(xtraname)
-                << " and extra precedence " << Rps_QuotedC_String(xtraprecedence));
-  /*** TODO:
-   * We need to create the instance of _55Z5Wgzuprq01MU6Br //repl_binary_operator∈class
-   ****/
+                << " and  precedence " << precedence);
   _f.obnewoper =
     Rps_ObjectRef::make_object(&_,
                                _f.obclassoper,
                                Rps_ObjectRef::root_space());
-  std::lock_guard<std::recursive_mutex> gunewoper(*_f.obnewoper->objmtxptr());
-  /** TODO:
-   * we need to fill obnewoper and register it as a root or as a constant
-   */
+  std::lock_guard<std::recursive_mutex>
+    gunewoper(*_f.obnewoper->objmtxptr());
   if (xtraname)
     {
       _f.strname = Rps_Value{std::string(xtraname)};
@@ -133,14 +136,16 @@ rps_do_plugin(const Rps_Plugin* plugin)
     }
   if (precedence >= 0)
     {
-      _f.obnewoper->put_attr(RPS_ROOT_OB(_7iVRsTR8u3D00Cy0hp), //repl_precedence∈symbol
-                             Rps_Value::make_tagged_int(precedence));
+      _f.obnewoper
+	->put_attr(RPS_ROOT_OB(_7iVRsTR8u3D00Cy0hp), //repl_precedence∈symbol
+		   Rps_Value::make_tagged_int(precedence));
     }
   if (comment != nullptr)
     {
       _f.strcomment = Rps_StringValue(comment);
-      _f.obnewoper->put_attr(RPS_ROOT_OB(_0jdbikGJFq100dgX1n), //comment∈symbol
-                             _f.strcomment);
+      _f.obnewoper
+	->put_attr(RPS_ROOT_OB(_0jdbikGJFq100dgX1n), //comment∈symbol
+		   _f.strcomment);
     }
   if (isrooted)
     {
