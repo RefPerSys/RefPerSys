29d617484f411828a630a4905cf85de5f908859a
Basile STARYNKEVITCH <basile@starynkevitch.net>
2026-08-04 08:26:28 +0200

    Abhishek Chakravarti is now a past author
    Signed-off-by: Basile STARYNKEVITCH <basile@starynkevitch.net>

--- backtrace_rps.cc
+++ backtrace_rps.cc
@@ -9,10 +9,11 @@
  *      from https://github.com/ianlancetaylor/libbacktrace
  *
  * Author(s):
- *      Basile Starynkevitch <basile@starynkevitch.net>
- *      Abhishek Chakravarti <abhishek@taranjali.org>
- *      Nimesh Neema <nimeshneema@gmail.com>
- *      Niklas Rozencrantz
+ *      Basile Starynkevitch, France   <basile@starynkevitch.net>
+ *      Niklas Rozencrantz, Sweden     <niklasr@protonmail.com>
+ *
+ * Past indian authors:       (no more interested after summer 2026)
+ *      Abhishek Chakravarti,    Nimesh Neema
  *
  *      © Copyright (C) 2020 - 2026 The Reflective Persistent System Team
  *      team@refpersys.org & http://refpersys.org/
