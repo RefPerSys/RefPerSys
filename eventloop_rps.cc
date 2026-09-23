fa43a891318a161ab17194915b98ac8358babf63
Basile STARYNKEVITCH <basile@starynkevitch.net>
2026-09-23 10:49:44 +0200

    typo in initial comment of eventloop_rps.cc
    Signed-off-by: Basile STARYNKEVITCH <basile@starynkevitch.net>

--- eventloop_rps.cc
+++ eventloop_rps.cc
@@ -34,7 +34,7 @@
  *
  *    You should have received a copy of the GNU General Public License
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- ***************************************************************************0***/
+ ***************************************************************************/
 
 
 #include "refpersys.hh"
